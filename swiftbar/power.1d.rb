c5b9e8e7686846c49e43b0ce7583e2a174bb3fd8
Lê Hùng Thiện <soloist_funk0b@icloud.com>
2021-12-21 11:19:41 +0700

    Update power.1d.rb
    Use cpc server for better stability and performance

--- swiftbar/power.1d.rb
+++ swiftbar/power.1d.rb
@@ -1,40 +1,47 @@
-#!/usr/bin/env /opt/homebrew/opt/ruby/bin/ruby
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'open-uri'
-require 'nokogiri'
 require 'date'
+require 'net/http'
+require 'uri'
+require 'json'
 
 # Class represents a single power cutoff
 class PowerCutoff
-  attr_reader :url, :date, :day_relative, :start_time, :end_time, :station_name, :reason
+  attr_reader :url, :date, :day_relative, :start_time, :end_time, :station_name, :station_code, :reason
 
   def found?
     !@row.nil?
   end
 
-  def initialize(station_query)
-    find_power_cutoff_row(station_query)
+  def initialize(customer_code)
+    find_power_cutoff_row(customer_code)
   end
 
   private
 
-  def text_at(selector)
-    @row.at_css(selector).text
-  end
-
-  def find_power_cutoff_row(station_query)
-    @url = 'https://ithongtin.com/lich-cup-dien/da-nang/cam-le'
-    @row = Nokogiri::HTML(URI.open(@url))
-                   .at_xpath("//*[@id='myTable']/tbody/tr[td[5]//text()[contains(., '#{station_query}')]]")
+  def find_power_cutoff_row(customer_code)
+    @row = request(customer_code)
     return unless found?
 
-    @date = text_at('td:nth-child(2)')
-    @day_relative = parse_relative_day(Date.parse(@date) - Date.today)
-    @start_time = text_at('td:nth-child(3)')
-    @end_time = text_at('td:nth-child(4)')
-    @station_name = text_at('td:nth-child(6)')
-    @reason = text_at('td:nth-child(7)').gsub(/\R+/, '\n ')
+    @date = Date.parse(@row['fromDate'])
+    @day_relative = parse_relative_day(@date - Date.today)
+    @start_time = DateTime.parse(@row['fromDate'])
+    @end_time = DateTime.parse(@row['toDate'])
+    @station_name = @row['stationName']
+    @station_code = @row['stationCode']
+    @reason = @row['reason']
+  end
+
+  def request(customer_code)
+    url = URI.parse("https://cskh-staging-api.cpc.vn:8080/api/remote/outages/#{customer_code}?viewReason=true")
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+    # Only care about the first power cut-off found
+    JSON.parse(response.read_body)[0]
   end
 
   def parse_relative_day(num_of_day)
@@ -52,23 +59,24 @@ end
 def put_power_cutoff_info(args)
   puts ":lightbulb.slash.fill: #{args.day_relative} | size=15"
   puts '---'
-  puts "Power cut-off for #{args.station_name} on #{args.date} from #{args.start_time} to #{args.end_time} | href=#{args.url}"
-  puts args.reason.to_s
+  puts "Power cut-off for station #{args.station_name} (#{args.station_code}) on #{args.date.strftime('%d %b %Y')}"
+  puts "From: #{args.start_time.strftime('%I:%M %p')}"
+  puts "To: #{args.end_time.strftime('%I:%M %p')}"
+  puts "Reason: #{args.reason}"
 end
 
-def check_power_cut_off_for(station_query)
-  power_cutoff = PowerCutoff.new(station_query)
+def check_power_cut_off_for(customer_code)
+  power_cutoff = PowerCutoff.new(customer_code)
   return unless power_cutoff.found?
 
   put_power_cutoff_info(power_cutoff)
-  # Only care about the first power cut-off found
   exit
 end
 
-station_query = ENV['STATION_QUERY'] || 'Hoà Cầm 4'
-check_power_cut_off_for(station_query)
+customer_code = ENV['CUSTOMER_CODE']
+check_power_cut_off_for(customer_code)
 
 # Fall back if no power-cutoff found
 puts ':lightbulb: | size=15'
 puts '---'
-puts 'No power cutoffs found for the next seven days | href="https://ithongtin.com/lich-cup-dien/da-nang/cam-le"'
+puts 'No power cutoffs found"'
